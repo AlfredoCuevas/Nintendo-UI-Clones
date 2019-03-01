dc21fe7cd5ad41242fcd94d6f245f13a12cbbefe
Alfredo Cuevas <cuevasalfredo0@gmail.com>
2019-03-01 15:52:21 -0800

    created the pivot that shows positions on a sphere, started kingdoms selector script

---
+++ Assets/KingdomSelect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingdomSelect : MonoBehaviour
+{
+    [SerializeField]
+    private List<Kingdom> _kingdoms = new List<Kingdom>();
+
+    [SerializeField]
+    private Transform _sphere;
+
+    [SerializeField]
+    private Transform _kingdomPivotPrefab;
+
+    private void Start()
+    {
+        foreach (Kingdom kingdom in _kingdoms)
+        {
+            SpawnKingdomPoint(kingdom);
+        }
+    }
+
+    private void SpawnKingdomPoint(Kingdom kingdom)
+    {
+        Transform myKingdom = Instantiate(_kingdomPivotPrefab, _sphere);
+    }
+
+    [System.Serializable]
+    public class Kingdom
+    {
+        public string name;
+        public float xAngle, yAngle;
+    }
+}
