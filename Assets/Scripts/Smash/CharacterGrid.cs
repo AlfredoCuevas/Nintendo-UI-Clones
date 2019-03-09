ea01fd4c2b55017f285e8393045f1184ec0aa4ec
Alfredo Cuevas <cuevasalfredo0@gmail.com>
2019-03-09 14:45:19 -0800

    created a scriptable object to hold character information, on start the grid instantiates a player cell with this information

---
+++ Assets/Scripts/Smash/CharacterGrid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterGrid : MonoBehaviour
+{
+    [SerializeField]
+    private List<Character> _characters = new List<Character>();
+
+    [SerializeField]
+    private GameObject _cellPrefab;
+
+    private void Start()
+    {
+        foreach (var character in _characters)
+        {
+            CreateCharacterCell(character);
+        }
+    }
+
+    private void CreateCharacterCell(Character character)
+    {
+        CharacterCellComponents cell = Instantiate(_cellPrefab, transform).GetComponent<CharacterCellComponents>();
+
+        cell.characterName.text = character.characterName; 
+        cell.artwork.sprite = character.characterSprite;
+    }
+}
