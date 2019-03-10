077c5a053553ddde25d328abac5c4631bb5bc487
Alfredo Cuevas <cuevasalfredo0@gmail.com>
2019-03-09 18:06:37 -0800

    changed the canvas to Screen Spcace - Camera, added the mouse moving script

---
+++ Assets/CursorDetection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CursorDetection : MonoBehaviour
+{
+    private GraphicRaycaster _graphicRaycaster;
+    private PointerEventData _pointerEventData = new PointerEventData(null);
+
+    void Start()
+    {
+        _graphicRaycaster = GetComponentInParent<GraphicRaycaster>();
+    }
+
+    void Update()
+    {
+        _pointerEventData.position = Camera.main.WorldToScreenPoint(transform.position);
+        List<RaycastResult> results = new List<RaycastResult>();
+        _graphicRaycaster.Raycast(_pointerEventData, results);
+
+        if(results.Count > 0)
+        {
+            Debug.Log(results[0].gameObject.name);
+        }
+    }
+}
---
+++ Assets/CursorMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 15f;
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(x, y, 0) * Time.deltaTime * _speed;
+
+        Vector3 worldSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -worldSize.x, worldSize.x),
+            Mathf.Clamp(transform.position.y, -worldSize.y, worldSize.y),
+            transform.position.z);
+    }
+}
