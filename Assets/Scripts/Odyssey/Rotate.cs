9ee4e4b54f5a2ffb940cdbea07ae9e7166a823a1
Alfredo Cuevas <cuevasalfredo0@gmail.com>
2019-03-15 21:33:14 -0700

    added a rotation script to madke the clouds move around the globe, made a folder for Odyssey scripts

---
+++ Assets/Scripts/Odyssey/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed = 1f;
+
+    private void Update()
+    {
+        transform.Rotate(0, _rotationSpeed, 0, Space.Self);
+    }
+}
