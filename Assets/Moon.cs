1deb12f9aa31f5dd6fb5835da989d7171a167946
Alfredo Cuevas <cuevasalfredo0@gmail.com>
2019-03-17 22:11:30 -0700

    added a moon into the scene that rotates around the earth

---
+++ Assets/Moon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, _rotationSpeed * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, new Vector3(0, 1, 0), _rotationSpeed * Time.deltaTime);
+    }
+}
